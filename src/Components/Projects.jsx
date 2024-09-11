3ca9f166c438deaabf5c22560dcca4a745cda719
SAICSP <chennasaicsp@gmail.com>
2024-09-11 22:22:01 +0530

    small change

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -9,7 +9,7 @@ function Projects() {
                     <img src="public/wiki.png" alt="wiki image" style={{height:"200px",width:"50%",borderRadius:"20px"}} />
                 <h3 style={{color:"#fff"}} className='mt-3'>WikiPedia</h3>
                 <p className="project-description">
-                    Developed a comprehensive Wikipedia-like web application that tackles issues related to text size, color, and font styling. The project features a responsive design, allowing for a user-friendly experience on both desktop and mobile devices. It demonstrates my ability to create well-structured, accessible, and visually appealing web content.
+                    Developed a compreh ensive Wikipedia-like web application that tackles issues related to text size, color, and font styling. The project features a responsive design, allowing for a user-friendly experience on both desktop and mobile devices. It demonstrates my ability to create well-structured, accessible, and visually appealing web content.
                 </p>
                 <h6>Demo: <a href="https://wikieditedbysai.netlify.app/" target="_blank" rel="noopener noreferrer">View Live Demo</a></h6>
             </div>  </div>
